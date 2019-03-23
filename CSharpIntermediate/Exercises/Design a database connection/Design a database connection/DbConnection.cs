c326b1b67b834c4b74caabb0c60a7cca761c35f8
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-03-23 09:41:27 +0000

    Added connection string validator

---
+++ CSharpIntermediate/Exercises/Design a database connection/Design a database connection/DbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Design_a_database_connection
+{
+    public class DbConnection
+    {
+
+        private string _connectionString;
+        private TimeSpan timeout;
+
+        public DbConnection(string connectionString)
+        {
+            _connectionString = ConnectionStringValidator(connectionString);
+        }
+
+        private string ConnectionStringValidator(string connString)
+        {
+            if (connString is null || connString.Length == 0)
+                throw new ArgumentException("Connection string cannot be null or empty");
+            else
+                return connString;
+        }
+    }
+}
