341e1b8e7a21eed8e87f6a3a05e4d20567449f20
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-03-30 19:33:58 +0000

    Got it. Finally.

--- CSharpAdvanced/Events/EventsTutorial/EventsTutorial/Program.cs
+++ CSharpAdvanced/Events/EventsTutorial/EventsTutorial/Program.cs
@@ -5,6 +5,7 @@ namespace EventsTutorial
     class Program
     {
         public delegate string MyDele(string i);
+
         static void Main(string[] args)
         {
 
@@ -14,14 +15,13 @@ namespace EventsTutorial
             VideoEncoder videoEncoder = new VideoEncoder();
             MailService mailService = new MailService();
 
-            // subscribing mailservice's handler to videoencoders event
             videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
             videoEncoder.Encode(video);
 
             // delegate reminder, nothing to do with events
-            MyDele quickDelegate = new MyDele(mailService.ChatFirst);
-            quickDelegate += mailService.ChatSecond;
-            quickDelegate("hi");
+            // MyDele quickDelegate = mailService.ChatFirst; // or do = new MeDele(method here);
+            // quickDelegate += mailService.ChatSecond;
+            // quickDelegate("hi");
             Console.ReadLine();
         }
     }
