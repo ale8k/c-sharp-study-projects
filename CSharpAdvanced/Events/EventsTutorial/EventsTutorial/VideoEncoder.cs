2fac5e0b891129d508abbb39291884e9e47d158c
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-03-30 15:01:35 +0000

    update

--- CSharpAdvanced/Events/EventsTutorial/EventsTutorial/VideoEncoder.cs
+++ CSharpAdvanced/Events/EventsTutorial/EventsTutorial/VideoEncoder.cs
@@ -5,10 +5,27 @@ namespace EventsTutorial
 {
     public class VideoEncoder
     {
+        /* MAKING EVENTS:
+         * Define our delegate
+         * Define our event (based on our delegate)
+         * Call our event in the method we want
+         */
+
+        // no idea why we send object:src, event args makes sense
+        public delegate void VideoEncodedEventHandler(object src, EventArgs args); 
+        public event VideoEncodedEventHandler VideoEncoded;
+
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video...");
             Thread.Sleep(3000);
+            OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
         }
     }
 }
