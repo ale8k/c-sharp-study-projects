dc0577e93e196a7e20692e31dcde16d30ca5b4d8
Alexander Kilroy <alexanderkilroy@outlook.com>
2019-04-04 15:25:11 +0100

    solution add

---
+++ XsAndOs/XsAndOs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XsAndOs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
