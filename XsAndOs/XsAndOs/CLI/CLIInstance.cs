82084cf80fa4e2ed5a0cceb36f8c820ab1182eeb
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-04-06 10:41:54 +0100

    update

--- XsAndOs/XsAndOs/CLI/CLIInstance.cs
+++ XsAndOs/XsAndOs/CLI/CLIInstance.cs
@@ -4,13 +4,17 @@ namespace XsAndOs.CLI
 {
     public class CLIInstance
     {
+        // All fields contain command references
+        // command refs are their own classes
         private Help _help = new Help();
 
         public CLIInstance()
         {
             Console.WriteLine("CLI instance successfully created");
         }
-
+        /*
+         * Our 'ui' of sorts... Lol 
+         */
         public void Commands(string usersInput)
         {
             string _usersInput = usersInput.ToLower();
