a88d6a1badd3dab14d010a623623f2df6658c2f1
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-04-06 16:38:34 +0100

    update

---
+++ Maze/Maze/GameInstance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Maze
+{
+    public class GameInstance
+    {
+        public DateTime GameStarted = DateTime.Now;
+
+        public bool GameIsActive { get; set; }
+        private int _arrayAmount;
+        private int _arrayLength;
+
+        public GameInstance(bool activeOrNot, int lanes, int howLong)
+        {
+            GameIsActive = activeOrNot;
+            _arrayAmount = lanes;
+            _arrayLength = howLong;
+
+            int[,] y = CreateGameArray(_arrayAmount, _arrayLength);
+            FillArray(y);
+
+        }
+
+        public int[,] CreateGameArray(int lanes, int howLong)
+        {
+            int arrayAmount = lanes;
+            int arrayLength = howLong;
+
+            int[,] gameArray = new int[arrayAmount, arrayLength];
+                                        // { 
+                                        //   {1,2,3}
+                                        //   {1,2,3}
+                                        //   {1,2,3}     
+                                        // }
+            return gameArray;
+        }
+
+        public void FillArray(int[,] array)
+        {
+            for (int i = 0; i < _arrayAmount; i++)
+            {
+                for (int j = 0; j < _arrayLength; j++)
+                {
+                    array[i, j] = j + 1;
+                }
+            }
+        }
+    }
+}
--- Maze/Maze/Program.cs
+++ Maze/Maze/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Maze.CLI;
 
 namespace Maze
 {
@@ -10,12 +7,15 @@ namespace Maze
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string usersInput = "";
+            CLIObj cli = new CLIObj();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            while (true)
+            {
+                usersInput = Console.ReadLine();
+                cli.Command(usersInput);
+
+            }
         }
     }
 }
