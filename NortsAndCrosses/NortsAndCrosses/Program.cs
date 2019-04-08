c4f8367659be89bd4816d8943aa386952d02297b
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-04-08 12:50:40 +0100

    improvement

--- NortsAndCrosses/NortsAndCrosses/Program.cs
+++ NortsAndCrosses/NortsAndCrosses/Program.cs
@@ -8,13 +8,11 @@ namespace NortsAndCrosses
         {
             Game game = null;
             string playersInput;
-            bool playerIDInitialised = false;
-            int playerID = 0; // dont wanna do this, feels weird af
-
+            int playersTurn;
             do
             {
-
                 playersInput = Console.ReadLine();
+
                 /*
                  * Handles commands outside of the game
                  */
@@ -29,30 +27,23 @@ namespace NortsAndCrosses
                         Console.WriteLine("Game ended.");
                         break;
                 }
+
                 /*
                  * Listens for a game start
                  */
                 if (game != null)
                 {
-                    // mhm, thought maybe having a bool check would work...
-                    if (playerIDInitialised == false)
-                    {
-                        playerID = game.GetPlayersTurn();
-                        playerIDInitialised = true;
-                    }
-
-                    Console.WriteLine($"It is player {playerID}'s turn");
+                    playersTurn = game.GetPlayersTurn();
+                    Console.WriteLine($"it is player {playersTurn}'s turn");
 
                     while (!game.ValidatePlayersInput(playersInput))
                     {
                         playersInput = Console.ReadLine();
                     }
-                    Console.WriteLine("accepted input");
-                    playerID = game.GetPlayersTurn();
-                    Console.WriteLine($"it is now player {playerID}'s turn");
+
                 }
 
-            } while (playerIDInitialised);
+            } while (true);
 
         }
     }
