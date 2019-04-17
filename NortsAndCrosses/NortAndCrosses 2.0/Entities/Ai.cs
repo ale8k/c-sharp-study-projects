597817e406ab7b1ac109aa077476161827e3b751
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-04-17 23:09:59 +0100

    logic notes

--- NortsAndCrosses/NortAndCrosses 2.0/Entities/Ai.cs
+++ NortsAndCrosses/NortAndCrosses 2.0/Entities/Ai.cs
@@ -17,7 +17,18 @@ namespace NortAndCrosses_2._0.Entities
 
         public void TakeTurn(List<int> gameMap)
         {
-            // get comp input, use method below to just validate everything is ok for sure
+            // Input = compLogic(); // delegate of comp logic methods?
+
+            // comp logic should check if a pattern is missing a piece of its pattern
+            // if it is, then that is the next choice for the computer
+            // pattern set must be built up of say 123, 147, 456 etc etc
+            // compare comp list, to game list, what pattern does comp have and is it available
+            // in the game list to complete, if yes, choose this
+            // if no, try next pattern
+
+            // TODO: clean this logic up and implement
+
+            // WITH comp input, use method below to just validate everything is ok for sure
 
             // does the input the computer has done exist in game map??
             GameInstance.ValidInputForMap(gameMap, 0); // something to think about
