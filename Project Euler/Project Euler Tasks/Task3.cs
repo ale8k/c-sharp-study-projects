230f4dee0573aa251af0d04bdd909987d7064f74
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-04-26 13:01:10 +0100

    3 task placeholders added

---
+++ Project Euler/Project Euler Tasks/Task3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project_Euler_Tasks
+{
+    public class Task3 : ITask
+    {
+        public int TaskID { get; } = 3;
+
+        public ITask ShowInfo()
+        {
+            Console.WriteLine("Largest prime factor:");
+
+            return this;
+        }
+
+        public void Run()
+        {
+        }
+    }
+}
---
+++ Project Euler/Project Euler Tasks/Task5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project_Euler_Tasks
+{
+    public class Task5 : ITask
+    {
+        public int TaskID { get; } = 5;
+
+        public ITask ShowInfo()
+        {
+            Console.WriteLine("A Task:");
+
+            return this;
+        }
+
+        public void Run()
+        {
+        }
+    }
+}
