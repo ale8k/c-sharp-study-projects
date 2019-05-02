28b16be5df4350af9aebc5a11486ed5585ea1f8e
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-05-02 15:11:38 +0100

    file info edit being taken

--- CustomerRegisterApp/CustomerRegisterApp/CLI/Commands/Edit.cs
+++ CustomerRegisterApp/CustomerRegisterApp/CLI/Commands/Edit.cs
@@ -1,23 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CustomerRegisterApp.CLI.Commands
 {
     public class Edit : ICommand
     {
+        private int _areaToEdit;
         public string CommandName { get; } = "edit";
 
         public void RunCommand()
         {
+            Console.WriteLine("Enter the customer file name you would like to edit: (e.g., Customer2)");
+
             Console.WriteLine("Please enter area you would like edit in the file:");
             Console.WriteLine("First name: 'first name', second name: 'second name', Mobile: 'tel' or Date of birth: 'dob'");
-            int area = GetAreaToEdit();
-            Console.WriteLine(area);
+            _areaToEdit = GetAreaToEdit();
+            Console.WriteLine(_areaToEdit);
             // split file into sub strings at the |
             // edit area according to array index, done. fappy hays
+        }
+        private void GetFileToEdit()
+        {
+            FileInfo fi1 = new FileInfo(path)
         }
 
         private int GetAreaToEdit()
