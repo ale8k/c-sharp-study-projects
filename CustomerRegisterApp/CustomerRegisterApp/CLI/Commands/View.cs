9c1757fe5315d0b2857d17d21d9c7d0fae53cb9a
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2019-05-02 17:21:20 +0100

    update

---
+++ CustomerRegisterApp/CustomerRegisterApp/CLI/Commands/View.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRegisterApp.CLI.Commands
+{
+    public class View : ICommand
+    {
+        public string CommandName { get; } = "edit";
+
+        public void RunCommand()
+        {
+            
+        }
+    }
+}
